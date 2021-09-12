3e88d9ec3bfd0523b4eb81058c14355fc4cc8b96
egormalejko <trukerboss@gmail.com>
2021-09-12 13:25:37 +0300

    добавлен класс принцессы

---
+++ princess.py
@@ -0,0 +1,8 @@
+class Princess:
+    def __init__(self, number, name, age, haircolor, eyecolor):
+        self.number = number
+        self.name = name
+        self.age = age
+        self.haircolor = haircolor
+        self.eyecolor = eyecolor
+
